1d50c55836f89babbde20c36c09c12cc41cdfade
jahedullah <jahedulislam6060@gmail.com>
2022-12-27 13:41:00 +0600

    User Login Restful

---
+++ src/main/java/com/Jahedullah/ProjectV1/URL/ProductURL.java
@@ -0,0 +1,12 @@
+package com.Jahedullah.ProjectV1.URL;
+
+public class ProductURL {
+
+    public static final String PRODUCT_WITH_ID = "/ProductsWithId/{courseId}";
+    public static final String PRODUCT_LIST = "/ProductsList";
+    public static final String PRODUCT_ADD = "/ProductsAdd";
+    public static final String PRODUCT_DELETE_BY_ID = "/ProductsDelete/{courseId}";
+    public static final String PRODUCT_UPDATE_BY_ID = "/ProductsUpdate/{courseId}";
+
+
+}
