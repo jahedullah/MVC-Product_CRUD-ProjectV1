24cb219e37aacc6c608684339dc44ad9e38aa32e
jahedullah <jahedulislam6060@gmail.com>
2023-01-05 11:25:40 +0600

    Added Userclass. Added Lombok dependency.

--- src/main/java/com/Jahedullah/ProjectV1/entity/User.java
+++ src/main/java/com/Jahedullah/ProjectV1/entity/User.java
@@ -1,67 +1,27 @@
 package com.Jahedullah.ProjectV1.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
-@Table(name = "users")
+@Table(name = "user")
 public class User {
     @Id
-    private String username;
-    private String password;
-    private boolean enabled;
-    private String role;
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private  String firstName;
+    private  String lastName;
+    private  String email;
+    private  String password;
+    private  Integer mobileNumber;
 
-    public User() {
-    }
-
-    public User(String username, String password, boolean enabled, String role) {
-        this.username = username;
-        this.password = password;
-        this.enabled = enabled;
-        this.role = role;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public boolean isEnabled() {
-        return enabled;
-    }
-
-    public void setEnabled(boolean enabled) {
-        this.enabled = enabled;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "username='" + username + '\'' +
-                ", password='" + password + '\'' +
-                ", enabled=" + enabled +
-                ", role='" + role + '\'' +
-                '}';
-    }
 }
