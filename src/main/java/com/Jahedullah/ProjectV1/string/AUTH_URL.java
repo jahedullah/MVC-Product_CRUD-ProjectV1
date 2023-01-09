6053642b5ad8e45d4db78a005c427d1d4e725219
jahedullah <jahedulislam6060@gmail.com>
2023-01-09 18:03:31 +0600

    Added SUPER_ADMIN Role. Can only create admin. Added logback files. AUTH_URl class

---
+++ src/main/java/com/Jahedullah/ProjectV1/string/AUTH_URL.java
@@ -0,0 +1,8 @@
+package com.Jahedullah.ProjectV1.string;
+
+public class AUTH_URL {
+    public static final String AUTHENTICATE = "/Authenticate";
+    public static final String USER_REGISTRATION = "/Register/User";
+    public static final String ADMIN_REGISTRATION = "/Register/Admin";
+
+}
